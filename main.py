9be7d86f28622f051caa31f8d9836349bcdd071a
jlescano1905 <jlescanog1@upao.edu.pe>
2026-06-20 06:51:50 -0500

    Update main.py 0.55

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
 
-    results = model(image, imgsz=640, conf=0.2)
+    results = model(image, imgsz=640, conf=0.55)
     result = results[0]
 
     detecciones = []
